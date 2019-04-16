c6e20f39419729df9862e61576e68fb5cc3ac0f7
ilya stasko <istsko@gmail.com>
2019-04-16 18:48:23 +0700

    Added FetchFeedService (internet connection & parsing)

---
+++ app/src/main/java/com/example/focusstartrssreader/FetchFeedService.java
@@ -0,0 +1,162 @@
+package com.example.focusstartrssreader;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FetchFeedService extends Service {
+
+    private static final String TAG = "FetchFeedService";
+    private String urlLink; // ссылка на новостную ленту
+    private ExecutorService executorService;
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        urlLink = intent.getStringExtra(MainActivity.FEED_URL);
+        executorService = Executors.newFixedThreadPool(1);
+        FetchFeedRun feedRun = new FetchFeedRun(startId);
+        executorService.execute(feedRun);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    class FetchFeedRun implements Runnable {
+
+        int startId;
+        private List<RssFeedModel> feedModelList;
+
+        public FetchFeedRun(int startId) {
+            this.startId = startId;
+        }
+        @Override
+        public void run() {
+
+            fetchFeedTask();
+            stop(startId);
+        }
+
+        boolean fetchFeedTask () {
+            if(TextUtils.isEmpty(urlLink))
+                return false;
+
+            try {
+                if(!urlLink.startsWith("http://") && !urlLink.startsWith("https://"))
+                    urlLink = "http://" + urlLink;
+
+                URL url = new URL(urlLink);
+                InputStream inputStream = url.openConnection().getInputStream();
+                feedModelList = parseFeed(inputStream);
+                    return true;
+            }
+            catch (IOException e) {
+                    Log.e(TAG, "Error", e);
+            }
+            catch (XmlPullParserException e) {
+                    Log.e(TAG, "Error", e);
+            }
+            return false;
+        }
+        //Парсинг RSS ленты
+        List<RssFeedModel> parseFeed(InputStream inputStream) throws XmlPullParserException, IOException {
+
+            String title = null;
+            String link = null;
+            String description = null;
+            boolean isItem = false;
+            List<RssFeedModel> items = new ArrayList<>();
+
+            try {
+
+                XmlPullParser xmlPullParser = Xml.newPullParser();
+                // FEATURE_PROCESS_NAMESPACES определяет, как будет парситься пространство имен XML
+                // Если задано false, пространства имен XML не будут обрабатываться
+                // и будут рассматриваться как обычные атрибуты
+                xmlPullParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                xmlPullParser.setInput(inputStream, null);
+
+                // nextTag() вызывает next() и возвращает элемент,
+                // если это START_TAG или END_TAG, иначе вызывает исключение
+                // next() возвращает след элемент
+                xmlPullParser.nextTag();
+                while (xmlPullParser.next() != xmlPullParser.END_DOCUMENT) {
+
+                    // получаем тип текущего элемента (START_TAG, END_TAG, TEXT)
+                    int eventType = xmlPullParser.getEventType();
+
+                    // получаем имя текущего элемента
+                    String name = xmlPullParser.getName();
+                    if (name == null)
+                        continue;
+
+                    if(eventType == XmlPullParser.END_TAG) {
+                        if(name.equalsIgnoreCase("item")) {
+                            isItem = false;
+                        }
+                        continue;
+                    }
+
+                    if (eventType == XmlPullParser.START_TAG) {
+                        if (name.equalsIgnoreCase("item")) {
+                            isItem = true;
+                            continue;
+                        }
+                    }
+
+                    //Log.d("XmlParser", "Parsing name = " + name);
+
+                    String result = "";
+                    if (xmlPullParser.next() == XmlPullParser.TEXT) {
+                        result = xmlPullParser.getText();
+                        xmlPullParser.nextTag();
+                    }
+
+                    if (name.equalsIgnoreCase("title")) {
+                        title = result;
+                    } else if (name.equalsIgnoreCase("link")) {
+                        link = result;
+                    } else if (name.equalsIgnoreCase("description")) {
+                        description = result;
+                    }
+
+                    if (title != null && link != null && description != null) {
+                        if (isItem) {
+                            RssFeedModel item = new RssFeedModel(title, link, description);
+                            items.add(item);
+                        }
+                        title = null;
+                        link = null;
+                        description = null;
+                        isItem = false;
+                    }
+                }
+                return items;
+            }
+            finally {
+                inputStream.close();
+            }
+        }
+
+        void stop(int startId) {
+            stopSelf(startId);
+        }
+    }
+}
