61fa788a152352886f8aa1cecef4f91a532b1c18
ilya stasko <istsko@gmail.com>
2019-05-08 12:48:15 +0700

    Added new classes GetFeedFromDBHandler, GetFeedFromDBRunnable. Get the news feed from the database in GetFeedFromDBRunnable and fill RecyclerView in GetFeedFromDBHandler

---
+++ app/src/main/java/com/example/focusstartrssreader/handler/GetFeedFromDBHandler.java
@@ -0,0 +1,29 @@
+package com.example.focusstartrssreader.handler;
+
+import com.example.focusstartrssreader.RssFeedAdapter;
+import com.example.focusstartrssreader.activities.MainActivity;
+
+import java.lang.ref.WeakReference;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.widget.RecyclerView;
+
+public class GetFeedFromDBHandler extends Handler {
+
+    private WeakReference<MainActivity> activity;
+
+    public GetFeedFromDBHandler(MainActivity activity) {
+        this.activity = new WeakReference<MainActivity>(activity);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        super.handleMessage(msg);
+        MainActivity mainActivity = activity.get();
+        if(mainActivity != null) {
+            RecyclerView recyclerView = mainActivity.getRecyclerView();
+            recyclerView.setAdapter((RssFeedAdapter) msg.obj);
+        }
+    }
+}
